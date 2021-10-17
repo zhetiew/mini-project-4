069821793b8af29d42786b6b43dd6d8f256351c3
zhetiew <zhetiew@gmail.com>
2021-10-17 07:43:42 +0000

    changes1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Color from '$lib/Color.svelte';
+    let colorName = 'Pink';
+</script>
+
+<div>
+    <h1 class="text-primary">Play with Colors</h1>
+</div>
+
+<div>
+    <input bind:value={colorName} />
+    <Color color={colorName} />
+</div>
+
+<style>
+    input {
+   	 padding: 0.5em;
+   	 text-align: center;
+    }
+    div {
+   	 text-align: center;
+   	 margin: 3em auto;
+    }
+</style>
